1d908abedfdc0b511a8ba7ae93652337cc479c04
Sharan-IITM <21f2000620@ds.study.iitm.ac.in>
2023-03-21 14:33:53 +0530

    Structuring the repo

---
+++ code/main.py
@@ -0,0 +1,30 @@
+import os
+from flask import Flask
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Resource,Api
+from Backend.database import db
+from flask_security import Security, SQLAlchemySessionUserDatastore, SQLAlchemyUserDatastore
+from Backend.models import User,Role
+from Backend.config import *
+
+current_dir = os.path.abspath(os.path.dirname(__file__))
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(current_dir, 'database.sqlite3')
+app.config.from_object(LocalDevelopmentConfig)
+db.init_app(app)
+api = Api(app)
+app.app_context().push()
+
+user_datastore =  SQLAlchemySessionUserDatastore(db.session,User,Role)
+
+security = Security(app,user_datastore)
+
+
+from Backend.api import *
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
