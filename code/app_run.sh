bba4cbc6bb245bd754692caa363452d8096e164b
Sharan-IITM <21f2000620@ds.study.iitm.ac.in>
2023-04-17 10:37:58 +0530

    port fix

--- code/app_run.sh
+++ code/app_run.sh
@@ -1,3 +1,3 @@
-/Users/Vincy/Desktop/soft-engg-project-jan-2023-group-3-1/code/main.py &
-cd /Users/Vincy/Desktop/soft-engg-project-jan-2023-group-3-1/code/frontend
-npm run serve
+.environment/bin/python main.py &
+cd ./frontend
+npm run serve -- --port 8082
